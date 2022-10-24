8603e0db694e4ca1eee67c7858ec7d9d62629c27
Antonio Osornio <a.osornio@outlook.com>
2022-10-23 22:10:42 -0500

    Maximo avance para entrega BootCamp
    Se agregan otras paginas como contacto, y se arreglan unos problemas de mediciones

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const iconoMenu = document.querySelector('.nav_menu');
 const navMenu = document.querySelector('.nav_list');
 
 iconoMenu.addEventListener('click', ()=>{
-    //navMenu.style.transform = 'translateX(0)';
+
     navMenu.classList.toggle('nav_list--show');
 
 });
